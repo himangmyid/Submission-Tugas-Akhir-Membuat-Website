e6dfdd64b9b38d414326667345af14cf8e3cc6c5
Himang.my.id <46196768+himangmyid@users.noreply.github.com>
2022-06-23 06:33:48 +0700

    Add files via upload

---
+++ himang/hi/javasc.js
@@ -0,0 +1,26 @@
+const txtElement = ['Blogger', 'Gamer', 'Streamer', 'Web Developer', 'Freelancer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function tik() {
+
+    if (count == txtElement.length) {
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-tik').textContent = words;
+
+    if (words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(tik, 500);
+
+
+})();
